a27353396e36ea1050f1b56877006395dc1fb3bb
Edoardo Vacchi <edoardo.vacchi@unicredit.eu>
2016-02-01 10:32:53 +0100

    refactor publisher

--- src/main/java/eu/unicredit/reactivestreams/kafka/AsyncIterablePublisher.java
+++ src/main/java/eu/unicredit/reactivestreams/kafka/AsyncIterablePublisher.java
@@ -11,13 +11,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
+ *
+ * NOTE: this is from org.reactivestreams.example. Code is licensed under CC0.
+ *       Class has been made package-private, since this is only an implementation detail.
+ *
+ *
  * AsyncIterablePublisher is an implementation of Reactive Streams `Publisher`
  * which executes asynchronously, using a provided `Executor` and produces elements
  * from a given `Iterable` in a "unicast" configuration to its `Subscribers`.
  *
  * NOTE: The code below uses a lot of try-catches to show the reader where exceptions can be expected, and where they are forbidden.
  */
-public class AsyncIterablePublisher<T> implements Publisher<T> {
+class AsyncIterablePublisher<T> implements Publisher<T> {
   private final static int DEFAULT_BATCHSIZE = 1024;
 
   private final Iterable<T> elements; // This is our data source / generator
--- src/main/java/eu/unicredit/reactivestreams/kafka/KafkaPublisher.java
+++ src/main/java/eu/unicredit/reactivestreams/kafka/KafkaPublisher.java
@@ -2,34 +2,37 @@ package eu.unicredit.reactivestreams.kafka;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.MockConsumer;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 import java.util.Iterator;
 import java.util.concurrent.Executor;
-import java.util.concurrent.atomic.AtomicLong;
 
-public class KafkaPublisher<K,V> extends AsyncIterablePublisher<ConsumerRecord<K,V>> {
+public class KafkaPublisher<K,V> implements Publisher<ConsumerRecord<K,V>> {
+    private final AsyncIterablePublisher<ConsumerRecord<K,V>> delegate;
+    private final Consumer<K, V> consumer;
+    private final long timeout;
+
     public KafkaPublisher(final Consumer<K, V> consumer, final long timeout, final Executor executor) {
-        super(() -> new Iterator<ConsumerRecord<K, V>>() {
-            Iterator<ConsumerRecord<K, V>> outstanding = requestMore();
-
-            @Override public boolean hasNext() { return true; }
-
-            @Override public ConsumerRecord<K, V> next() {
-                while(!outstanding.hasNext()) { requestMore(); }
-                return outstanding.next();
-            }
-
-            private Iterator<ConsumerRecord<K, V>> requestMore() {
-                return consumer.poll(timeout).iterator();
-            }
-        }, executor);
-
+        this.consumer = consumer;
+        this.timeout = timeout;
+        this.delegate = new AsyncIterablePublisher<>(KafkaIterator::new, executor);
     }
+
+    @Override public void subscribe(final Subscriber<? super ConsumerRecord<K, V>> subscriber) {
+        this.delegate.subscribe(subscriber);
+    }
+
+    class KafkaIterator implements Iterator<ConsumerRecord<K,V>> {
+        private Iterator<ConsumerRecord<K, V>> outstanding = requestMore();
+        @Override public boolean hasNext() { return true; }
+        @Override public ConsumerRecord<K, V> next() {
+            while(!outstanding.hasNext()) { requestMore(); }
+            return outstanding.next();
+        }
+        private Iterator<ConsumerRecord<K, V>> requestMore() { return consumer.poll(timeout).iterator(); }
+    };
+
 
 
 }
